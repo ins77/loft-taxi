d3fc9746a07fcbce1d59ecb633eece6b9e9d9375
d.sukhinin <d.sukhinin@tinkoff.ru>
2019-10-30 15:54:36 +0300

    improvement: added test files

--- src/components/App/App.test.ts
+++ src/components/App/App.test.ts
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// fix TypeError: window.URL.createObjectURL is not a function
+// https://github.com/mapbox/mapbox-gl-js/issues/3436
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({}),
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(React.createElement(App), div);
